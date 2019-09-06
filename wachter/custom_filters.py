03623d0a5335dc0190fe4828c629533b9514a43c
Nik Lorem <loremdevs@gmail.com>
2019-09-06 22:36:10 +0300

    Update custom_filters.py

--- wachter/custom_filters.py
+++ wachter/custom_filters.py
@@ -1,11 +1,11 @@
 from telegram.ext import BaseFilter
 
 
-class FilterAdded(BaseFilter):                                         # filter for message, that bot was added to group
+class FilterBotAdded(BaseFilter):                                         # filter for message, that bot was added to group
     def filter(self, message):
         if message.new_chat_members[-1].is_bot:
             return False
         return True
 
 
-filter_added = FilterAdded()
+filter_bot_added = FilterBotAdded()
