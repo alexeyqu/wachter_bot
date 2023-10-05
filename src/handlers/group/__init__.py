bfa2974e1781fc952e74b7629320be6cf2150bed
Alex Kulikov <7394728+alexeyqu@users.noreply.github.com>
2023-10-05 16:29:54 -0700

    nice group chat nux POC (#25)
    * nice group chat nux
    
    * nux

--- src/handlers/group/__init__.py
+++ src/handlers/group/__init__.py
@@ -3,3 +3,4 @@ Module with all the telegram handlers related to group chat flow.
 """
 
 from .group_handler import on_hashtag_message, on_new_chat_member
+from .my_chat_member_handler import my_chat_member_handler
