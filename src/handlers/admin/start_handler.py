f1006d255a86e4aecb51c97c6e3e3208f7824c26
Alexey Kulikov <alexeyqu@gmail.com>
2023-10-17 21:35:08 +0100

    hack to avoid slow /start

--- src/handlers/admin/start_handler.py
+++ src/handlers/admin/start_handler.py
@@ -1,31 +1,24 @@
 import json
-import logging
 from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import CallbackContext
-from typing import List
 
 from src import constants
-from src.model import Chat, session_scope
+from src.model import User, session_scope
 
 from .utils import authorize_user
 
 
-logger = logging.getLogger(__name__)
-
 # todo @admin decorator to prevent / tweak behaviour when calling from group chats
 # this will be a nice replacement for "if user_id < 0" checks
 def start_handler(update: Update, context: CallbackContext):
-    # TODO replace with update.message.from_user.id when we have @admin decorator
     user_id = update.message.chat_id
 
     if user_id < 0:
         return
 
     with session_scope() as sess:
-        # TODO slow, need to rethink the DB schema to get all user's chats efficiently without the whois requirement
-        # nullable whois field? would that cause problems?
-        chats = sess.query(Chat).all()
-        user_chats = _filter_chats(chats, user_id, context.bot)
+        users = sess.query(User).filter(User.user_id == user_id)
+        user_chats = list(_get_chats(users, user_id, context.bot))
 
     if len(user_chats) == 0:
         update.message.reply_text("У вас нет доступных чатов.")
@@ -48,15 +41,13 @@ def start_handler(update: Update, context: CallbackContext):
     update.message.reply_text(constants.on_start_command, reply_markup=reply_markup)
 
 
-def _filter_chats(chats: List[Chat], user_id: int, bot: Bot) -> List[Chat]:
-    filtered_chats = []
-    for chat in chats:
+def _get_chats(users: list, user_id: int, bot: Bot):
+    for x in users:
         try:
-            if authorize_user(bot, chat.id, user_id):
-                filtered_chats.append({
-                    "title": bot.get_chat(chat.id).title or chat.id,
-                    "id": chat.id,
-                })
-        except Exception as e:
-            logger.error(e)
-    return filtered_chats
+            if authorize_user(bot, x.chat_id, user_id):
+                yield {
+                    "title": bot.get_chat(x.chat_id).title or x.chat_id,
+                    "id": x.chat_id,
+                }
+        except Exception:
+            pass
