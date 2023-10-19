97a161e55226f3f2ed4dd0f5e921c7398eb69ae9
Lev <l.e.chechulin@gmail.com>
2023-10-19 18:42:22 +0200

    Do things better, faster, stronger

--- src/handlers/admin/utils.py
+++ src/handlers/admin/utils.py
@@ -1,4 +1,9 @@
-from telegram import Bot
+import json
+from telegram import Bot, InlineKeyboardButton
+from telegram.ext import CallbackContext
+
+from src.model import Chat, User, session_scope
+from src import constants
 
 
 def authorize_user(bot: Bot, chat_id: int, user_id: int):
@@ -7,3 +12,36 @@ def authorize_user(bot: Bot, chat_id: int, user_id: int):
         return status in ["creator", "administrator"]
     except Exception:
         return False
+
+
+def get_chats_list(user_id, context: CallbackContext):
+    with session_scope() as sess:
+        users = sess.query(User).filter(User.user_id == user_id)
+        return _get_chats_helper(users, user_id, context.bot)
+
+
+def _get_chats_helper(users: list, user_id: int, bot: Bot):
+    for x in users:
+        try:
+            if authorize_user(bot, x.chat_id, user_id):
+                yield {
+                    "title": bot.get_chat(x.chat_id).title or x.chat_id,
+                    "id": x.chat_id,
+                }
+        except Exception:
+            pass
+
+
+def create_chats_list_keyboard(user_chats, context: CallbackContext, user_id):
+    return [
+        [
+            InlineKeyboardButton(
+                chat["title"],
+                callback_data=json.dumps(
+                    {"chat_id": chat["id"], "action": constants.Actions.select_chat}
+                ),
+            )
+        ]
+        for chat in user_chats
+        if authorize_user(context.bot, chat["id"], user_id)
+    ]
