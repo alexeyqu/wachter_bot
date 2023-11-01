da37ca73f61d7c8960bd3a878cee113b680ad0ad
Alexey Kulikov <alexeyqu@gmail.com>
2023-11-01 22:51:08 +0000

    Revert "temporary log team ids"
    This reverts commit 29a5db1f28fd8f53cea76a04fc2a46e7a7ead739.

--- app.py
+++ app.py
@@ -61,9 +61,7 @@ def main():
 
     updater.start_polling()
     tg_logger.info("Bot has started successfully")
-    tg_logger.info(os.environ.get("TEAM_TELEGRAM_IDS", "[]"))
     updater.idle()
-    
 
 
 if __name__ == "__main__":
