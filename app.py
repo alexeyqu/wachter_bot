8e49cfa4dc991fc1a84114941e22cf3a7a8ba302
Alex Kulikov <alexeyqu@gmail.com>
2023-12-11 00:07:19 +0000

    misc

--- app.py
+++ app.py
@@ -22,12 +22,13 @@ def main():
         .token(os.environ["TELEGRAM_TOKEN"])
         .build()
     )
-    application.job_queue.scheduler.add_jobstore(
-        PTBSQLAlchemyJobStore(
-            application=application,
-            url=os.environ["PERSISTENCE_DATABASE_URL"],
+    if "PERSISTENCE_DATABASE_URL" in os.environ:
+        application.job_queue.scheduler.add_jobstore(
+            PTBSQLAlchemyJobStore(
+                application=application,
+                url=os.environ["PERSISTENCE_DATABASE_URL"],
+            )
         )
-    )
 
     application.add_handler(CommandHandler("help", handlers.help_handler))
     application.add_handler(CommandHandler("listjobs", handlers.list_jobs_handler))
