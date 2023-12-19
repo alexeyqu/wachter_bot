9400082e7160a3d25f4bd709f0105360fbb948d9
Alex Kulikov <alexeyqu@gmail.com>
2023-12-19 04:20:29 +0000

    feat: otlp & uptrace monitoring

--- src/logging.py
+++ src/logging.py
@@ -2,6 +2,31 @@ import logging
 from logging import config
 import os
 
+import grpc
+from opentelemetry._logs import set_logger_provider
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import (
+    OTLPLogExporter,
+)
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+from opentelemetry.sdk.resources import Resource
+
+dsn = os.environ.get("UPTRACE_DSN")
+
+resource = Resource(
+    attributes={"service.name": "wachter-bot", "service.version": "1.1.0"}
+)
+logger_provider = LoggerProvider(resource=resource)
+set_logger_provider(logger_provider)
+
+exporter = OTLPLogExporter(
+    endpoint="otlp.uptrace.dev:4317",
+    headers=(("uptrace-dsn", dsn),),
+    timeout=5,
+    compression=grpc.Compression.Gzip,
+)
+logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
+
 log_config = {
     "version": 1,
     "disable_existing_loggers": False,
@@ -10,6 +35,11 @@ log_config = {
             "class": "telegram_logger.TelegramHandler",
             "token": os.environ["TELEGRAM_TOKEN"],
             "chat_ids": [os.environ["TELEGRAM_ERROR_CHAT_ID"]],
+        },
+        "wachter_oltp": {
+            "class": "opentelemetry.sdk._logs.LoggingHandler",
+            "level": logging.INFO,
+            "logger_provider": logger_provider,
         }
     },
     "loggers": {
@@ -17,6 +47,7 @@ log_config = {
             "level": "INFO",
             "handlers": [
                 "wachter_telegram",
+                "wachter_oltp",
             ],
         }
     },
