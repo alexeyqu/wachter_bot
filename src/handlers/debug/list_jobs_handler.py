2ae461683fd1913bdfddaa5b6df1359076728827
Alex Kulikov <alexeyqu@gmail.com>
2024-02-22 20:46:41 +0000

    fix: return all jobs if listjobs arg is none

--- src/handlers/debug/list_jobs_handler.py
+++ src/handlers/debug/list_jobs_handler.py
@@ -11,7 +11,7 @@ from src.handlers.utils import debug
 async def list_jobs_handler(update: Update, context: CallbackContext) -> None:
     args = update.effective_message.text.split()
     chat_id = int(args[1]) if len(args) > 1 else None
-    jobs = [job for job in context.job_queue.jobs() if job.data.get("chat_id") == chat_id]
+    jobs = [job for job in context.job_queue.jobs() if chat_id is None or job.data.get("chat_id") == chat_id]
     await update.message.reply_text(
         f"<b>Jobs: {len(jobs)} items</b>\n\n"
         + "\n\n".join(
