b8ee6885805508f297de49a1358a39fbc6b15174
Alex Kulikov <alexeyqu@gmail.com>
2025-01-19 09:41:19 +0000

    fix: log final message_mention for debug

--- src/handlers/group/group_handler.py
+++ src/handlers/group/group_handler.py
@@ -351,11 +351,11 @@ async def _mention_markdown(bot: Bot, chat_id: int, user_id: int, message: str) 
     #user_mention_markdown = user_mention_markdown.replace("/[", "[")
     #user_mention_markdown = user_mention_markdown.replace("]", "\]")
     # wtf
-    tg_logger.warning(message.replace("%USER\\\\\\_MENTION%", user_mention_markdown))
     message_mention = message.replace("%USER\\\\\\_MENTION%", user_mention_markdown)
     message_mention = message_mention.replace("%USER\\\\_MENTION%", user_mention_markdown)
     message_mention = message_mention.replace("%USER\\_MENTION%", user_mention_markdown)
     message_mention = message_mention.replace("%USER_MENTION%", user_mention_markdown)
+    tg_logger.warning(message_mention)
     return message_mention
 
 
